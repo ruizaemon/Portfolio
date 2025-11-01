218e84dc9233a23c0c242ac1dc1af6f5a2e978c5
ruizaemon <ruimail.jp@gmail.com>
2025-11-01 17:08:39 +0900

    Add @ path alias, add hero title translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   i18n: {
     locales: ['en', 'ja'],
     defaultLocale: 'en',
-  }
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  },
 });
